d8303bd2fe96d24192b2b95663d1ee88841baa61
Miguel <moreneitorsoftware@gmail.com>
2019-03-18 20:27:51 +0100

    Finished

--- src/Principal.java
+++ src/Principal.java
@@ -4,25 +4,26 @@ public class Principal {
 
     public static void main(String[] args) {
 
+        ArrayList<Figura> listaDeFiguras = getFiguras();
+
+        recorre("Paralelogramo",listaDeFiguras);
+        recorre("Figura",listaDeFiguras);
+
+    }
+
+    private static ArrayList<Figura> getFiguras() {
         ArrayList<Figura> listaDeFiguras = new ArrayList<>();
 
         Figura triangulo = new Triangulo("Triangulo");
-        Figura cuadrado = new Rombo("Cuadrado");
-        Figura rombo = new Cuadrado("Rombo");
-
-        String tipo;
-
+        Figura cuadrado = new Cuadrado("Cuadrado");
+        Figura rombo = new Rombo("Rombo");
 
         listaDeFiguras.add(triangulo);
         listaDeFiguras.add(cuadrado);
         listaDeFiguras.add(triangulo);
         listaDeFiguras.add(cuadrado);
         listaDeFiguras.add(rombo);
-
-
-        recorre("Paralelogramo",listaDeFiguras);
-        recorre("Figura",listaDeFiguras);
-
+        return listaDeFiguras;
     }
 
     private static void recorre(String tipo, ArrayList<Figura> listaDeFiguras) {
